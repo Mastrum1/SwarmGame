90563de52800b94c69f5595a2a617eaaf670a33a
Lior Yaari <93042006+Lint111@users.noreply.github.com>
2025-03-26 09:09:02 +0100

    Added external github projects to fasten development process

---
+++ Assets/Flocking/Scripts/Boid/BoidMovement_Rigidbody.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//handles movement of the boid using a rigidbody
+[RequireComponent(typeof(Rigidbody))]
+public class BoidMovement_Rigidbody : BoidMovement
+{
+    private Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public override void MoveBoid(Vector3 vel)
+    {
+        vel = LimitVelocity(vel, maxSpeed);
+        rb.AddForce(vel);
+        rb.linearVelocity = LimitVelocity(rb.linearVelocity, maxSpeed);
+        if(rb.linearVelocity != Vector3.zero) transform.forward = rb.linearVelocity;
+    }
+
+    public override Vector3 GetVelocity()
+    {
+        return rb.linearVelocity;
+    }
+
+    //Limit a vector's magnitude to a certain limit if it is over that limit
+    private Vector3 LimitVelocity(Vector3 velocity, float velocityLimit)
+    {
+        float velMagnitude = velocity.magnitude;
+
+        if (velMagnitude > velocityLimit)
+        {
+            return (velocity / velMagnitude) * velocityLimit;
+        }
+        else
+        {
+            return velocity;
+        }
+    }
+}
